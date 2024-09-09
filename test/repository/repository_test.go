2a629e95de9cff1d9ed5b7298bc5d0f94f02c359
Ricky.You <ricky.you@bjss.com>
2024-09-09 10:42:14 +0100

    Add an example test for repository

--- test/repository/repository_test.go
+++ test/repository/repository_test.go
@@ -1,10 +1,14 @@
 package repository_test
 
 import (
-	"testing"
 	"context"
+	"testing"
 
+	"academy/todoapp/datastore"
 	"academy/todoapp/internal/db"
+	"academy/todoapp/internal/model"
+
+	cmp "github.com/google/go-cmp/cmp"
 )
 
 var ctx context.Context = context.WithValue(context.TODO(), "fakeKey", "fakeID")
@@ -33,6 +37,40 @@ func TestRWMutexLock(t *testing.T) {
 		want := "listName - list3 taken, please change" 
 		if got != want  {
 			t.Errorf("got %s, want %s", got, want)
+		}
+	})
+}
+
+func TestCreateTodos(t *testing.T) {
+	t.Run("Successfully create todos", func(t *testing.T) {
+		datastore := datastore.Store
+		err := db.CreateTodos(ctx, "list3", []string{"yoga", "boxing"})
+
+		if err != nil {
+			t.Fatalf("Fail to create: %s", err.Error())
+		}
+
+		got, ok := datastore.UserTodos["list3"]
+		want := model.TodoList{
+			ToDos: []*model.Todo{
+				{
+					Description: "yoga",
+					Status: "planned",
+				},
+				{
+					Description: "boxing",
+					Status: "planned",
+				},
+			},
+			Note: "",
+		}
+
+		if !ok {
+			t.Error("Todo not created")
+		}
+
+		if !cmp.Equal(*got, want) {
+			t.Errorf("got %v, want %v", got, want)
 		}
 	})
 }
